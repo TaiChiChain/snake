006f5dfa82886254970b017dc6f656cebfed4534
jiuhuche120 <760297795@qq.com>
2023-07-04 17:46:50 +0800

    feat(*): build structrue

---
+++ tests/block.test.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@jest/globals'
+import { client } from './heper'
+
+test('eth_getBlockByHash', async () => {
+    const blockNumber = await client.eth.getBlockNumber()
+    let block = await client.eth.getBlock(blockNumber)
+    expect(block.number).toBe(blockNumber)
+
+    const blockHash = block.hash
+    block = await client.eth.getBlock(blockHash)
+    expect(block.number).toBe(blockNumber)
+})
+
+test('eth_getBlockByNumber', async () => {
+    const blockNumber = await client.eth.getBlockNumber()
+    const block = await client.eth.getBlock(blockNumber)
+    expect(block.number).toBe(blockNumber)
+})
+
+test('eth_getBlockTransactionCountByHash', async () => {
+    const blockNumber = await client.eth.getBlockNumber()
+    const block = await client.eth.getBlock(blockNumber)
+    expect(block.number).toBe(blockNumber)
+
+    const count = await client.eth.getBlockTransactionCount(block.hash)
+    expect(count).toBe(BigInt(block.transactions.length))
+})
---
+++ tests/heper.ts
@@ -0,0 +1,4 @@
+import Web3 from 'web3'
+
+const url = "http://localhost:8545"
+export const client = new Web3(url)
