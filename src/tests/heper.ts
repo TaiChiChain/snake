ddf55030392283ae411106ceb0a9249f89088fdc
liweixia <790890362@qq.com>
2023-07-10 11:48:03 +0800

    feat: add contracts

--- src/tests/heper.ts
+++ src/tests/heper.ts
@@ -2,7 +2,7 @@ import Web3 from 'web3'
 import { compile } from '../utils/compile';
 import { ST_STORAGE_FILENAME, ST_STORAGE_CONTRACT_NAME } from '../utils/contracts';
 
-const url = "http://localhost:8545"
+const url = "http://172.23.82.60:8881"
 export const client = new Web3(url)
 
 compile(ST_STORAGE_FILENAME, ST_STORAGE_CONTRACT_NAME)
