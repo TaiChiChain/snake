b8935ac9da7b1e4c9ccb77f42d15c8ea080c1b11
jiuhuche120 <760297795@qq.com>
2023-07-13 13:58:17 +0800

    feat(helper): add eth_newFilter, eth_newBlockFilter, eth_newPendingTransactionFilter

--- src/tests/heper.ts
+++ src/tests/heper.ts
@@ -1,6 +1,57 @@
 import Web3 from 'web3'
-import { compile } from '../utils/compile';
-import { ST_STORAGE_FILENAME, ST_STORAGE_CONTRACT_NAME } from '../utils/contracts';
+import axios from 'axios';
 
-const url = "http://localhost:8881"
+const url = "http://127.0.0.1:8881"
 export const client = new Web3(url)
+
+interface ethRequest {
+    id: number
+    jsonrpc: string
+    method: string
+    params: any
+}
+
+export async function call(method: string, params?: any) {
+    const request: ethRequest = {
+        id: 1,
+        jsonrpc: '2.0',
+        method: method,
+        params: params
+    }
+
+    const body = JSON.stringify(request)
+    console.log(body);
+
+    try {
+        const result = await axios.post(url, body, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        return result.data
+    } catch (error: any) {
+        throw new Error(error.message)
+    }
+}
+
+declare type BlockTag = 'latest' | 'pending' | 'earliest'
+declare type HexString = string
+
+export interface FilterParams {
+    fromBlock?: HexString | BlockTag
+    toBlock?: HexString | BlockTag
+    address?: HexString[]
+    topics?: HexString[][]
+}
+
+export async function newFilter(params: FilterParams[]) {
+    return await call('eth_newFilter', params)
+}
+
+export async function newBlockFilter() {
+    return await call('eth_newBlockFilter')
+}
+
+export async function newPendingTransactionFilter() {
+    return await call('eth_newPendingTransactionFilter')
+}
