f308cd988e56cdf40f4b11b382a3008297705899
heyifeng <heyifeng@pku.edu.cn>
2023-10-17 15:08:11 +0800

    fix: kyc verify test case

--- src/tests/access_tests/kycverification.test.ts
+++ src/tests/access_tests/kycverification.test.ts
@@ -1,16 +1,17 @@
 import {test, expect, describe} from '@jest/globals'
-import {ethers, id} from '@axiomesh/axiom'
+import {ethers, Wallet} from '@axiomesh/axiom'
 import {
     ST_CONTRACT_DIR,
     ST_ACCESS_KYC_ADDRESS,
 } from "../../utils/contracts_static";
 import {provider} from "../../utils/rpc";
-import {ST_ADMIN_1, ST_ACCOUNT_1, ST_ACCOUNT_2, ST_ADMIN_2, ST_ADMIN_3} from "../../utils/accounts_static";
-import {hexStringToString, hexToString, stringToUint8Array} from "../../utils/util";
+import {ST_ADMIN_1} from "../../utils/accounts_static";
+import {stringToUint8Array} from "../../utils/util";
 import fs from "fs";
 
 describe('test case for kyc verification', () => {
     describe('test normal submit and remove', () => {
+        const randomWallet = Wallet.createRandom();
         const Verified = 1
         const Unverified = 0
         const expires = Math.floor(Date.now() / 1000 + 1000);
@@ -18,17 +19,17 @@ describe('test case for kyc verification', () => {
         const extraSubmitArgs = {
             KycInfos: [
                 {
-                    User: "0x264e23168e80f15e9311F2B88b4D7abeAba47E54",
+                    User: randomWallet.address,
                     KycAddr: ST_ADMIN_1.address,
                     KycFlag: Verified,
                     Expires: expires,
                 },
             ]
         }
-        test ('test account1 cannot send raw transaction', async () => {
-            let wallet = new ethers.Wallet(ST_ADMIN_1.privateKey, provider);
+        test ('test user1 cannot send raw transaction', async () => {
+            const wallet = new ethers.Wallet(ST_ADMIN_1.privateKey, provider);
             let txDetails = {
-                to: ST_ACCOUNT_1.address,
+                to: randomWallet.address,
                 value: ethers.parseEther('1000'),
                 gasLimit: 21000,
             };
@@ -36,43 +37,42 @@ describe('test case for kyc verification', () => {
             let receipt = await txResponse.wait();
             expect(receipt?.status).toBe(1)
 
-            wallet = new ethers.Wallet("3331caf8f8638ae16403666e6d8a2f29bbf40a2f780ae5781134bac05c93e890", provider);
             txDetails = {
-                to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+                to: ST_ADMIN_1.address,
                 value: ethers.parseEther('100'),
                 gasLimit: 21000,
             };
 
             try {
-                txResponse = await wallet.sendTransaction(txDetails);
+                txResponse = await randomWallet.sendTransaction(txDetails);
                 receipt = await txResponse.wait();
-                expect(receipt?.status).toBe(0)
+                expect(receipt?.status).toBe(1)
             } catch (error) {
+                // eslint-disable-next-line no-console
                 console.log("error is", error)
             }
         })
 
-        test('test set account1 kycinfo verified', async () => {
-            let wallet = new ethers.Wallet(ST_ADMIN_1.privateKey, provider)
-            let contract = new ethers.Contract(ST_ACCESS_KYC_ADDRESS, abi, wallet);
-            let toByte = stringToUint8Array(JSON.stringify(extraSubmitArgs));
+        test('test set user1 kycinfo verified', async () => {
+            const wallet = new ethers.Wallet(ST_ADMIN_1.privateKey, provider)
+            const contract = new ethers.Contract(ST_ACCESS_KYC_ADDRESS, abi, wallet);
+            const toByte = stringToUint8Array(JSON.stringify(extraSubmitArgs));
             const submit = await contract.Submit(
                 toByte
             );
             await submit.wait()
-            let tx = await provider.getTransactionReceipt(submit.hash)
+            const tx = await provider.getTransactionReceipt(submit.hash)
             expect(tx?.status).toBe(1)
         })
 
-        test ('test account1 can send saw transaction', async () => {
-            const wallet = new ethers.Wallet("3331caf8f8638ae16403666e6d8a2f29bbf40a2f780ae5781134bac05c93e890", provider);
+        test ('test user1 can send saw transaction', async () => {
             const txDetails = {
-                to: ST_ACCOUNT_2.address,
+                to: ST_ADMIN_1.address,
                 value: ethers.parseEther('10'),
                 gasLimit: 21000,
             };
 
-            const txResponse = await wallet.sendTransaction(txDetails);
+            const txResponse = await randomWallet.sendTransaction(txDetails);
             const receipt = await txResponse.wait();
             expect(receipt?.status).toBe(1)
         })
